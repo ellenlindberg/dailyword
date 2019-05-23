e165c2e1a864bfbf7847e95fbce93632a91b17ad
elinforsnor <elin.forsnor@gmail.com>
2019-05-23 13:43:35 +0200

    Refactorated Switch and filestructure

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Header from './header/Header';
-import Main from './main/Main';
+import Root from './routes/root/Root';
+import FavouriteWords from './routes/favouritewords/FavouriteWords';
 import Footer from './footer/Footer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+        <div class="container">
+          <Switch>
+            <Route exact path="/" component={Root} />
+            <Route path="/favouritewords" component={FavouriteWords} />
+          </Switch>
+        </div>
+        
+      <Footer />
+    </div>
+  );
 }
+
 
 export default App;
